83ef14563cb0c734f18ec22cf3aa14392c5b6c51
Armando Rodríguez <jarodriguezvera@gmail.com>
2023-03-29 12:20:18 -0600

    Lectura de matriz

---
+++ Multidimensional.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FormsMultidimensional_2023_II
+{
+    class Multidimensional : Matriz
+    {
+        private float[,] A;
+
+        public Multidimensional(int M, int N )
+        {
+            this.M = M;
+            this.N = N;
+            A = new float[this.M, this.N];
+        }
+
+        public static Multidimensional Leer(string A)
+        {
+            //1,2,3;4,5,6
+            string[] renglones = A.Split(';');
+            //renglones[0]= {1,2,3};
+            //renglones[1]= {4,5,6};
+            string[] columnas = renglones[0].Split(',');
+
+
+        }
+    }
+}
